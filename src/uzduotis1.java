ecd4222815262ef3c17570d8069c9b7b950b02b9
audriusxp <palm@inbox.lt>
2022-07-01 20:35:29 +0300

    sukurta funkcija priimanti skaicius bandau sukurti funkcija kuri iskrirtu didesnius skaicius nei 12 ir juos surasytu i nauja sarasa, bet niekaip neavyksta

--- src/uzduotis1.java
+++ src/uzduotis1.java
@@ -4,27 +4,48 @@ import java.util.Scanner;
 
 public class uzduotis1 {
     public static void main(String[] args) {
-        ArrayList<Double>masyvas=nuskaitytiSarasa();
+        ArrayList<Double> masyvas = nuskaitytiSarasa();
         System.out.println("masyvas = " + masyvas);
 
 
     }
-    public static ArrayList<Double>nuskaitytiSarasa(){
+
+    public static ArrayList<Double> nuskaitytiSarasa() {
         Scanner skaitytuvas = new Scanner(System.in);
 
         ArrayList<Double> skaiciai = new ArrayList<>();
-        System.out.println("Iveskite " + skaiciai);
-        int n=skaitytuvas.nextInt() ;
+        System.out.println("Iveskite skaiciu is kurio susidarys masyvas " + skaiciai);
+        int n = skaitytuvas.nextInt();
         for (int i = 0; i < n; i++) {
-          skaiciai.add(skaitytuvas.nextDouble());
+            skaiciai.add(skaitytuvas.nextDouble());
         }
         return skaiciai;
-    }
-
-
 
 
     }
+
+    public static ArrayList<Double> daugiauDvylika() {
+        int x = 0;
+        for (int i = 0; i < nuskaitytiSarasa().size(); i++) {
+            if (x > 12) {
+                System.out.println("x = " + x);
+                return daugiauDvylika();
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
